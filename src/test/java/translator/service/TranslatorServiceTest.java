9a270b26496f7e1fe2461faa356a7ff3c5415f01
Julio Salceda <julio.salceda@cetys.edu.mx>
2019-11-07 19:04:58 +0100

    Added test functions for more languages

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -20,9 +20,21 @@ public class TranslatorServiceTest {
   TranslatorService translatorService;
 
   @Test
-  public void translateTest() {
+  public void translateEnToEs() {
     TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
     assertEquals("Esto es una prueba de servicio de traducción", translatedText.getTranslation());
+  }
+
+  @Test
+  public void translateEnToKo() {
+    TranslatedText translatedText = translatorService.translate("en", "ko", "This is a test translation from English to Korean.");
+    assertEquals("이것은 시험에서 영어 번역 한국어입니다.", translatedText.getTranslation());
+  }
+
+  @Test
+  public void translateEsToKo() {
+    TranslatedText translatedText = translatorService.translate("en", "sv", "This is a test translation from English to Swedish.");
+    assertEquals("Detta är ett test för översättning från engelska till svenska.", translatedText.getTranslation());
   }
 
 }
